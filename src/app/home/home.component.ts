6bfc646db364511ede303f17a07cb9307a2b73fe
Spyros Alertas <s.alertas@outlook.com>
2024-01-27 21:31:13 +0200

    Create brand component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { BrandComponent } from '../brand/brand.component';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BrandComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
