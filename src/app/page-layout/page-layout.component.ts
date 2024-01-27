4f3e16b36ca0370d50b9c3778b2a859311490795
Spyros Alertas <s.alertas@outlook.com>
2024-01-28 00:50:25 +0200

    Create footer component

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HomeComponent } from '../home/home.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-page-layout',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent],
+  imports: [NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
