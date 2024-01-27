e7107ec5632591eaa3ea16198f9b6bfbfe503988
Spyros Alertas <s.alertas@outlook.com>
2024-01-27 22:03:56 +0200

    Create section a component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { BrandComponent } from '../brand/brand.component';
+import { SectionAComponent } from '../section-a/section-a.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BrandComponent],
+  imports: [BrandComponent, SectionAComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/section-a/section-a.component.ts
@@ -0,0 +1,39 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-a',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './section-a.component.html',
+  styleUrl: './section-a.component.css'
+})
+export class SectionAComponent {
+
+  public cards: {
+    title: string,
+    content: string
+  }[] = [
+      {
+        title: 'Title 1',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, non.'
+      },
+      {
+        title: 'Title 2',
+        content: 'Lorem ipsum dolor sit amet consectetur.'
+      },
+      {
+        title: 'Title 3',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nam sunt esse impedit ad neque!'
+      },
+      {
+        title: 'Title 4',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ipsum incidunt. Itaque blanditiis esse doloribus accusantium voluptas quod, culpa sapiente.'
+      },
+      {
+        title: 'Title 5',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi nam sunt esse impedit ad neque!'
+      }
+    ];
+
+}
