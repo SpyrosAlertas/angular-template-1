6b54be521bbf2826d824555ada0ede9ce37173f4
Spyros Alertas <s.alertas@outlook.com>
2024-01-27 20:59:52 +0200

    Create home component layout

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { NavbarComponent } from '../navbar/navbar.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-page-layout',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, HomeComponent],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
