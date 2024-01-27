a725d7ea519711f390d7d8abc7825dd49b27842d
Spyros Alertas <s.alertas@outlook.com>
2024-01-27 22:52:16 +0200

    Create contact details component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { BrandComponent } from '../brand/brand.component';
 import { SectionAComponent } from '../section-a/section-a.component';
 import { SectionBComponent } from '../section-b/section-b.component';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BrandComponent, SectionAComponent, SectionBComponent],
+  imports: [BrandComponent, SectionAComponent, SectionBComponent, ContactDetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
