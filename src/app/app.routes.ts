68916171f5b5158ebfcd77be7065b15213838c4f
Spyros Alertas <s.alertas@outlook.com>
2024-01-28 11:59:56 +0200

    Add scroll support for navbar/header links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { ExtraOptions, InMemoryScrollingFeature, Routes, withInMemoryScrolling } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    { path: '', title: 'Home | Brand Name', component: HomeComponent }
+];
+
+const scrollConfig: ExtraOptions = {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+};
+
+export const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,35 +1,51 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbModule],
+  imports: [RouterLink, NgbModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  isNavbarCollapsed: boolean = true;
   private isClickIn: boolean = false;
 
+  isNavbarCollapsed: boolean = true;
+
+  constructor(private viewportScroller: ViewportScroller) {
+    this.viewportScroller.setOffset([0, 64]);
+  }
+
+  clickIn() {
+    this.isClickIn = true;
+  }
+
+  scroll() {
+    if (!this.isNavbarCollapsed) {
+      this.viewportScroller.setOffset([0, 215]);
+      this.isNavbarCollapsed = true;
+    } else {
+      this.viewportScroller.setOffset([0, 64]);
+    }
+  }
+
   @HostListener('window:resize')
-  onResize() {
+  private onResize() {
     this.isNavbarCollapsed = true;
   }
 
   @HostListener('document:click')
-  clickOut() {
+  private clickOut() {
     if (this.isClickIn) {
       this.isClickIn = false;
     } else {
       this.isNavbarCollapsed = true;
     }
-  }
-
-  clickIn() {
-    this.isClickIn = true;
   }
 
 }
