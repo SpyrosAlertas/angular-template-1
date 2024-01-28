a49a91cdae4cdcdb6e20d3c7a9eaad371a1a74f6
Spyros Alertas <s.alertas@outlook.com>
2024-01-28 12:10:08 +0200

    Fix in page layout to use router-outlet instead of home component directly

--- src/app/page-layout/page-layout.component.ts
+++ src/app/page-layout/page-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HomeComponent } from '../home/home.component';
@@ -7,7 +8,7 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-page-layout',
   standalone: true,
-  imports: [NavbarComponent, HomeComponent, FooterComponent],
+  imports: [NavbarComponent, HomeComponent, FooterComponent, RouterOutlet],
   templateUrl: './page-layout.component.html',
   styleUrl: './page-layout.component.css'
 })
