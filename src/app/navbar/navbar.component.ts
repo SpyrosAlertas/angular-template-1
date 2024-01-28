639d413a8f967db3eed03280ad56a5b7f03d986b
Spyros Alertas <s.alertas@outlook.com>
2024-01-28 13:58:00 +0200

    Add dropdown in navbar to change site colors and 4 new stylesheets

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,37 @@
-import { ViewportScroller } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { DOCUMENT, NgFor, ViewportScroller } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { ThemeModel } from './theme-model';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, NgbModule],
+  imports: [NgFor, RouterLink, NgbModule, NgbDropdownModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isNavbarCollapsed: boolean = true;
+  themeOptions: ThemeModel[] = [
+    { name: 'Simple B&W', filename: 'assets/stylesheets/theme-1.css' },
+    { name: 'Green', filename: 'assets/stylesheets/theme-2.css' },
+    { name: 'Brown', filename: 'assets/stylesheets/theme-3.css' },
+    { name: 'Purple', filename: 'assets/stylesheets/theme-4.css' },
+    { name: 'Blue', filename: 'assets/stylesheets/theme-5.css' }
+  ];
 
   private isClickIn: boolean = false;
 
-  isNavbarCollapsed: boolean = true;
-
-  constructor(private viewportScroller: ViewportScroller) {
+  constructor(private viewportScroller: ViewportScroller, @Inject(DOCUMENT) private document: Document) {
     this.viewportScroller.setOffset([0, 64]);
+  }
+
+  ngOnInit(): void {
+    this.changeTheme(this.themeOptions[0]);
   }
 
   clickIn() {
@@ -32,6 +45,16 @@ export class NavbarComponent {
     } else {
       this.viewportScroller.setOffset([0, 64]);
     }
+  }
+
+  changeTheme(theme: ThemeModel) {
+    const head = this.document.getElementsByTagName('head')[0];
+    const style = this.document.createElement('link');
+    style.id = 'client-theme';
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = theme.filename;
+    head.appendChild(style);
   }
 
   @HostListener('window:resize')
---
+++ src/app/navbar/theme-model.ts
@@ -0,0 +1,5 @@
+export class ThemeModel {
+
+    constructor(public filename: string, public name: string) { }
+
+}
