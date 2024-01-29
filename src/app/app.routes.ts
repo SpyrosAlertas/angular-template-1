b6e5609c1d3218eb26fca3d01f8440b64f0083ac
Spyros Alertas <s.alertas@outlook.com>
2024-01-29 21:00:07 +0200

    add page not found and route all unknown pages to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { ExtraOptions, InMemoryScrollingFeature, Routes, withInMemoryScrolling }
 
 import { HomeComponent } from './home/home.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '', title: 'Home | Brand Name', component: HomeComponent },
-    { path: 'privacy-policy', title: 'Privacy Policy | Brand Name', component: PrivacyPolicyComponent }
+    { path: 'privacy-policy', title: 'Privacy Policy | Brand Name', component: PrivacyPolicyComponent },
+    { path: '**', title: 'Page Not Found | Brand Name', component: PageNotFoundComponent }
 ];
 
 const scrollConfig: ExtraOptions = {
